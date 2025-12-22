6b25ae96a8139ee6ab5c1cf489343ca88753d671
nvamsi2015 <56445275+nvamsi2015@users.noreply.github.com>
2025-12-22 14:56:18 +0530

    Update commands.js

--- harkirat /notes/commands.js
+++ harkirat /notes/commands.js
@@ -11,3 +11,6 @@ node dist/index.js   // to run the application from  build file instead of using
 // ----------- to start the server using docker -------------
 
 create a file named Dockerfile in the root directory of your project and add the following code to it:
+
+// -------- deployment errors ----------
+https://nextjs.org/docs/messages/failed-to-find-server-action
