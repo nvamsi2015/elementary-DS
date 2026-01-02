60cee250a1d098193e7a23a6523a8f1ef00934bd
nvamsi2015 <56445275+nvamsi2015@users.noreply.github.com>
2026-01-02 23:43:36 +0530

    Create node-setup.ts

---
+++ 1javascript/node-setup.ts
@@ -0,0 +1,55 @@
+npm init 
+npx tsc --init
+// ------------- index.js--------
+
+import express from 'express';
+const app = express()
+const PORT = 3000;
+
+
+app.get('/',(req,res) =>{
+    console.log('received request', req)
+    res.send('Hello from Express home route!    ');
+
+})
+
+app.listen(PORT, ()=>{
+    console.log('Server is running on port', PORT)
+})
+
+
+
+// -------- tsconfig--------- 
+  "compilerOptions": {
+    // File Layout
+    "rootDir": "./src",
+    "outDir": "./dist",
+
+    // Environment Settings
+    // See also https://aka.ms/tsconfig/module
+    // "module": "nodenext",
+    "module": "esnext",
+    "target": "esnext",
+    "types": ["node"],
+    // For nodejs:
+    // "lib": ["esnext"],
+    // "types": ["node"],
+    // and npm install -D @types/node
+
+    // Other Outputs
+    "sourceMap": true,
+    "declaration": true,
+    "declarationMap": true,
+
+
+
+
+      // ---------- package.json --------
+
+  "scripts": {
+    "build": "tsc -b",
+    "start": "node ./dist/index.js",
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+
+
