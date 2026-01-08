9cd954c088c3c1b9f89d7a558fd020ef3a4ba865
nvamsi2015 <56445275+nvamsi2015@users.noreply.github.com>
2026-01-08 21:38:18 +0530

    expo-setup

---
+++ expo.ts
@@ -0,0 +1,5 @@
+npx create-expo-app@latest  => commands creates with default template app with (tabs) navigation, asks name( creates Home and explore bar at the bottom )
+npm i 
+npx expo start  => to start the project 
+
+
