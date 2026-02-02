03aabb77e9e7b378060a7433d60668559334c5cf
nvamsi2015 <nvamsi2015@gmail.com>
2026-02-02 12:26:05 +0530

    added git folder

---
+++ 12git/local_config.js
@@ -0,0 +1,32 @@
+
+//  to exit out of git console always use q
+
+// to work with diferent username and email for only that repo use this commands
+
+//--------- to check current user details
+
+// $ git config --global user.name
+// ng-vamsi
+
+// $ git config --global user.email
+// Vamsi.N@ngenux.com
+
+//----------- changing to personal
+//  git config --local user.name "nvamsi2015"
+//  git config --local user.email "nvamsi2015@gmail.com"
+
+//  checking 
+// $ git config --list --local
+// core.repositoryformatversion=0
+// core.filemode=false
+// core.bare=false
+// core.logallrefupdates=true
+// core.symlinks=false
+// core.ignorecase=true
+// remote.origin.url=https://github.com/nvamsi2015/elementary-DS.git
+// remote.origin.fetch=+refs/heads/*:refs/remotes/origin/*
+// branch.main.remote=origin
+// branch.main.merge=refs/heads/main
+// branch.main.vscode-merge-base=origin/main
+// user.name=nvamsi2015
+// user.email=nvamsi2015@gmail.com
