57524c6ebdc265907f4f70b7655de593c4c7ea4e
nvamsi2015 <nvamsi2015@gmail.com>
2026-03-24 12:03:47 +0530

    git file added

---
+++ 11terminal/git.py
@@ -0,0 +1,5 @@
+git merge:    :wq to exit out of merge console 
+git log:      q to exit out of the console
+
+#------- remove merged changes locally before committing and pushing -------
+ git reset --hard HEAD~1
